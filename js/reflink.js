f6d2478b12bed592585d0ca1ac5f9591ba798714
vasyltpieceofcak <vasyltpieceofcake@gmail.com>
2024-11-09 14:41:23 +0200

    Add files via upload

---
+++ js/reflink.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("a").click(function(){
+    $("#reflink*").attr("href", "https://partnervavadarv.com/?promo=77ab7c4c-6502-476a-a0e7-1a4fcb1b6b8e&target=register");
+  });
+});
