bad953314ebdaf19c18aec4f7f31f14c9c706e9d
vasyltpieceofcak <vasyltpieceofcake@gmail.com>
2024-11-10 12:43:57 +0200

    Delete js/reflink.js

--- js/reflink.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  $("a").click(function(){
-    $("#reflink*").attr("href", "https://partnervavadarv.com/?promo=77ab7c4c-6502-476a-a0e7-1a4fcb1b6b8e&target=register");
-  });
-});
